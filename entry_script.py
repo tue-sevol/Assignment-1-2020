f4d8dd6f7be331396816260246a4c51eddf93a9e
Nathan Cassee <n.w.cassee@tue.nl>
2020-01-23 11:48:49 +0100

    Add more comments

--- entry_script.py
+++ entry_script.py
@@ -6,6 +6,10 @@ import sys
 
 
 def write_output_file():
+'''
+Writes a dummy output file using the python csv writer, update this 
+to accept as parameter the found trace links. 
+'''
     with open('/output/links.csv', 'w') as csvfile:
 
         writer = csv.writer(csvfile, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
@@ -18,11 +22,11 @@ def write_output_file():
         writer.writerow(["UC1", "L1, L34, L5"]) 
         writer.writerow(["UC2", "L5, L4"]) 
 
+
+if __name__ == "__main__":
 '''
 Entry point for the script
 '''
-if __name__ == "__main__":
-
     if len(sys.argv) < 2:
         print("Please provide an argument to indicate which matcher should be used")
         exit(1)
@@ -33,13 +37,19 @@ if __name__ == "__main__":
         match_type = int(sys.argv[1])
     except ValueError as e:
         print("Match type provided is not a valid number")
-        exit(1)
-
-    
+        exit(1)    
 
     print(f"Hello world, running with matchtype {match_type}!")
 
+    # Read input low-level requirements and count them (ignore header line).
     with open("/input/low.csv", "r") as inputfile:
-        print(f"There are {len(inputfile.readlines()) - 1} requirements")
+        print(f"There are {len(inputfile.readlines()) - 1} low-level requirements")
+
+
+    '''
+    This is where you should implement the trace level logic as discussed in the 
+    assignment on Canvas. Please ensure that you take care to deliver clean,
+    modular, and well-commented code.
+    '''
 
     write_output_file()
